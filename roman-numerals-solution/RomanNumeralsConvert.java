d33a641d18635e5501f899ef970b502e35ed24d9
EnjoyLife <Alex.Geryavenko@gmail.com>
2017-09-15 14:39:58 +0300

    Implement convert algorithm

--- roman-numerals-solution/RomanNumeralsConvert.java
+++ roman-numerals-solution/RomanNumeralsConvert.java
@@ -12,11 +12,47 @@ public class RomanNumeralsConvert {
 				final String[] numArray = line.split("\\s+");
 
 				for (int i = 0; i < numArray.length; i++) {
-					System.out.println(numArray[i]);
+					System.out.println(convert(numArray[i]));
 				}
 			}
 		} catch (final IOException e) {
 			System.err.println("IOException reading System.in" + e);
 		}
+	}
+
+	public static String convert(String numStr) {
+		String Roman = "";
+
+		final String onesArray[] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+		final String tensArray[] = { "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+		final String hundredsArray[] = { "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+		int num = Integer.parseInt(numStr);
+
+		final int ones = num % 10;
+
+		num = (num - ones) / 10;
+		final int tens = num % 10;
+
+		num = (num - tens) / 10;
+		final int hundreds = num % 10;
+
+		num = (num - hundreds) / 10;
+		for (int i = 0; i < num; i++) {
+			Roman += "M";
+		}
+
+		if (hundreds >= 1) {
+			Roman += hundredsArray[hundreds - 1];
+		}
+
+		if (tens >= 1) {
+			Roman += tensArray[tens - 1];
+		}
+
+		if (ones >= 1) {
+			Roman += onesArray[ones - 1];
+		}
+
+		return String.valueOf(Roman);
 	}
 }
