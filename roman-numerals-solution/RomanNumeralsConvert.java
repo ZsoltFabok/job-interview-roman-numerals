0a0a499203fe857e008305012641df9c8eb6c62f
EnjoyLife <Alex.Geryavenko@gmail.com>
2017-09-18 13:38:34 +0300

    Rewrite convert solution
    using collection
    change convert algorithm
    exit if catch exception
    changed  methods access modifiers
    fix condition in if statement

--- roman-numerals-solution/RomanNumeralsConvert.java
+++ roman-numerals-solution/RomanNumeralsConvert.java
@@ -1,59 +1,83 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.TreeMap;
 
 public class RomanNumeralsConvert {
-	
-	public final static String onesArray[] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
-	public final static String tensArray[] = { "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
-	public final static String hundredsArray[] = { "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
 
 	public final static String TEST_MOD = "-test";
 	public final static String ONE = "I";
 	public final static String TWELVE = "XII";
 	public final static String ONE_HUNDRED_AND_THREE = "CIII";
 
-	private static String[] numArray;
+	private final static TreeMap<Integer, String> map = new TreeMap<>();
+	static {
+		map.put(1000, "M");
+		map.put(900, "CM");
+		map.put(500, "D");
+		map.put(400, "CD");
+		map.put(100, "C");
+		map.put(90, "XC");
+		map.put(50, "L");
+		map.put(40, "XL");
+		map.put(10, "X");
+		map.put(9, "IX");
+		map.put(5, "V");
+		map.put(4, "IV");
+		map.put(1, "I");
+	}
 
 	public static void main(String[] args) {
 
 		if (args.length > 0) {
 			if (args.length > 1 || !TEST_MOD.equals(args[0])) {
 				System.out.println("Only one flag supports: - test");
-			} else {
-				runTest();
 			}
+
+			runTest();
+
 		} else {
-			readInput();
+			final List<String> numbersList = readInput();
 
-			validate();
+			validate(numbersList);
 
-			convert();
+			convert(numbersList);
 
-			printResult();
+			printResult(numbersList);
 		}
 
 	}
 
-	public static void readInput() {
-		try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in));) {
+	private static List<String> readInput() {
+		final List<String> numbersList = new ArrayList<>();
+		try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
 			String line;
 			while ((line = in.readLine()) != null) {
 				line = line.replace("\"", "").trim();
-				numArray = line.split("\\s+");
+				numbersList.addAll(Arrays.asList(line.split("\\s+")));
 			}
+
 		} catch (final IOException e) {
 			System.err.println("IOException reading System.in" + e);
+			System.exit(1);
 		}
+
+		return numbersList;
 	}
 
-	public static void validate() {
-		for (int i = 0; i < numArray.length; i++) {
-			final String numStr = numArray[i];
+	private static void validate(List<String> numbersList) {
+		final ListIterator<String> listIterator = numbersList.listIterator();
+		while (listIterator.hasNext()) {
+			final String numStr = listIterator.next();
+
 			boolean e = true;
 			for (int k = 0; k < numStr.length(); k++) {
 				if (Character.isDigit(numStr.charAt(k)) == false) {
-					numArray[i] = numArray[i] + ", illegal argument error - is not valid digital number.";
+					listIterator.set(numStr + ", illegal argument error - is not valid digital number.");
 					e = false;
 					break;
 				}
@@ -62,103 +86,87 @@ public class RomanNumeralsConvert {
 			if (e) {
 				final int num = Integer.parseInt(numStr);
 				if (num > 3999 || num < 1) {
-					numArray[i] = numArray[i] + ", range bound error - number should be in the range [1 - 3999].";
+					listIterator.set(numStr + ", range bound error - number should be in the range [1 - 3999].");
 				}
 			}
 		}
 	}
 
-	public static void convert() {
-		for (int i = 0; i < numArray.length; i++) {
-			final String numStr = numArray[i];
+	private final static String toRoman(Integer number) {
+		final int l = map.floorKey(number);
+		if (number == l) {
+			return map.get(number);
+		}
+		return map.get(l) + toRoman(number - l);
+	}
+
+	private static void convert(List<String> numbersList) {
+		final ListIterator<String> listIterator = numbersList.listIterator();
+		while (listIterator.hasNext()) {
+			final String numStr = listIterator.next();
 
 			if (numStr.contains("error")) {
 				continue;
 			}
+			
+			final Integer num = Integer.valueOf(numStr);
 
-			String Roman = "";
-			int num = Integer.parseInt(numStr);
-
-			final int ones = num % 10;
-
-			num = (num - ones) / 10;
-			final int tens = num % 10;
-
-			num = (num - tens) / 10;
-			final int hundreds = num % 10;
-
-			num = (num - hundreds) / 10;
-			for (int n = 0; n < num; n++) {
-				Roman += "M";
+			final int l = map.floorKey(num);
+			if (num == l) {
+				listIterator.set(map.get(num));
+			} else {
+				listIterator.set(map.get(l) + toRoman(num - l));
 			}
-
-			if (hundreds >= 1) {
-				Roman += hundredsArray[hundreds - 1];
-			}
-
-			if (tens >= 1) {
-				Roman += tensArray[tens - 1];
-			}
-
-			if (ones >= 1) {
-				Roman += onesArray[ones - 1];
-			}
-
-			numArray[i] = String.valueOf(Roman);
 		}
 	}
 
-	public static void printResult() {
-		for (int i = 0; i < numArray.length; i++) {
-			System.out.println(numArray[i]);
-		}
-
+	public static void printResult(List<String> numbersList) {
+		numbersList.forEach(System.out::println);
 	}
 
 	public static void runTest() {
-		numArray = new String[5];
-		
-		numArray[0] = "1";
-		numArray[1] = "12";
-		numArray[2] = "103";
+		final List<String> numbersList = new ArrayList<>();
 
-		numArray[3] = "10char3";
-		numArray[4] = "10399911";
+		numbersList.add("1");
+		numbersList.add("12");
+		numbersList.add("103");
+		numbersList.add("10char3");
+		numbersList.add("10399911");
 
-		validate();
+		validate(numbersList);
 
-		convert();
+		convert(numbersList);
 
-		System.out.print("1 => " + numArray[0]);
-		if (ONE.equals(numArray[0])) {
+		System.out.print("1 => " + numbersList.get(0));
+		if (ONE.equals(numbersList.get(0))) {
 			System.out.println(" passed.");
 		} else {
 			System.out.println(" failed.");
 		}
 
-		System.out.print("12 => " + numArray[1]);
-		if (TWELVE.equals(numArray[1])) {
+		System.out.print("12 => " + numbersList.get(1));
+		if (TWELVE.equals(numbersList.get(1))) {
 			System.out.println(" passed.");
 		} else {
 			System.out.println(" failed.");
 		}
 
-		System.out.print("103 => " + numArray[2]);
-		if (ONE_HUNDRED_AND_THREE.equals(numArray[2])) {
+		System.out.print("103 => " + numbersList.get(2));
+		if (ONE_HUNDRED_AND_THREE.equals(numbersList.get(2))) {
 			System.out.println(" passed.");
 		} else {
 			System.out.println(" failed.");
 		}
 
-		System.out.print("10char3 => " + numArray[3]);
-		if ("10char3, illegal argument error - is not valid digital number.".equals(numArray[3])) {
+		System.out.print("10char3 => " + numbersList.get(3));
+		if ("10char3, illegal argument error - is not valid digital number.".equals(numbersList.get(3))) {
 			System.out.println(" passed.");
 		} else {
 			System.out.println(" failed.");
 		}
 
-		System.out.print("10399911 => " + numArray[4]);
-		if ("10399911, range bound error - number should be in the range [1 - 3999].".equals(numArray[4])) {
+		System.out.print("10399911 => " + numbersList.get(4));
+		if ("10399911, range bound error - number should be in the range [1 - 3999].".equals(numbersList.get(4))) {
 			System.out.println(" passed.");
 		} else {
 			System.out.println(" failed.");
